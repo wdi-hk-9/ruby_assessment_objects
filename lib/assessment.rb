ad7e951c02a42dd138b044e43b7ec6a359496997
Jacky Lee <jackywhlee@gmail.com>
2015-10-20 23:16:24 +0800

    done

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -1,39 +1,47 @@
 class Person
-
   def initialize (first_name, last_name, full_name)
     @first_name = first_name
     @last_name = last_name
     @full_name = full_name
   end
-
 end
 
 
 class Player < Person
-
-  attr_accessor :health
-
-  def initialize (health=20, strength=5, alive=true)
+  def initialize (first_name, last_name, full_name, health=20, strength=5, alive=true)
+    super(first_name, last_name, full_name)
     @health = health
+    @strength = strength
     @alive = alive
-    super
   end
 
   def take_damage(attack_strength)
-    health -= attack_strength
+    @health -= attack_strength
+    if @health <= 0
+      @alive = false
+      return "you dead m8. " + @health.to_s + "hp overkill"
+    else
+      return @health.to_s + "hp remaining"
+    end
   end
 
   def attack(player)
-
+    player.take_damage(@strength)
   end
-
 end
 
 
 class Knight < Player
+  def initialize (first_name, last_name, full_name, health=50, strength=7, alive=true)
+    super
+  end
+end
 
+class Wizard < Player
+  def initialize (first_name, last_name, full_name, health=20, strength=75, alive=true)
+    super
+  end
+end
 
-
-
-
-
+shittywizard = Wizard.new("Jacky", "Lee", "Jacky Lee")
+shittyknight = Knight.new("Denis", "Cheung", "Denis Cheung")
