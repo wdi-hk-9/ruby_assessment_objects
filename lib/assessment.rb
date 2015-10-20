00f17e5bc7b023ff0edec6bd4b1e85d34a1bf18e
Jacky Lee <jackywhlee@gmail.com>
2015-10-20 20:54:28 +0800

    starting from home

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -1,3 +1,39 @@
-# Write your answer bellow
-#
-# Happy coding!!
+class Person
+
+  def initialize (first_name, last_name, full_name)
+    @first_name = first_name
+    @last_name = last_name
+    @full_name = full_name
+  end
+
+end
+
+
+class Player < Person
+
+  attr_accessor :health
+
+  def initialize (health=20, strength=5, alive=true)
+    @health = health
+    @alive = alive
+    super
+  end
+
+  def take_damage(attack_strength)
+    health -= attack_strength
+  end
+
+  def attack(player)
+
+  end
+
+end
+
+
+class Knight < Player
+
+
+
+
+
+
